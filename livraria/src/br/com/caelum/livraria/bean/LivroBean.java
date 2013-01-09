4087a081858dddd6b50ca2e6b554e9b95affb030
Armando Couto <armando-couto@hotmail.com>
2013-01-09 04:51:13 -0300

    Curso finalizado.

--- livraria/src/br/com/caelum/livraria/bean/LivroBean.java
+++ livraria/src/br/com/caelum/livraria/bean/LivroBean.java
@@ -30,8 +30,13 @@ public class LivroBean {
 		System.out.println("Gravando livro " + this.livro.getTitulo());
 
 		if (livro.getAutores().isEmpty()) {
+<<<<<<< HEAD
+			FacesContext.getCurrentInstance().addMessage("autor",
+					new FacesMessage("Livro deve ter pelo menos um Autor"));
+=======
 			FacesContext.getCurrentInstance().addMessage("autor", new
 			FacesMessage("Livro deve ter pelo menos um Autor"));
+>>>>>>> branch 'master' of https://github.com/armando-couto/JSF2_Caelum.git
 		}
 
 		new DAO<Livro>(Livro.class).adiciona(this.livro);
@@ -41,6 +46,26 @@ public class LivroBean {
 			Object value) throws ValidatorException {
 		String valor = value.toString();
 		if (!valor.startsWith("1")) {
+<<<<<<< HEAD
+			throw new ValidatorException(new FacesMessage(
+					"Deveria começar com 1"));
+		}
+	}
+
+	public String formAutor() {
+		System.out.println("Chamanda o formulario do Autor");
+		return "autor?faces-redirect=true";
+	}
+
+	public Livro getLivro() {
+		return livro;
+	}
+
+	public List<Autor> getAutores() {
+		return new DAO<Autor>(Autor.class).listaTodos();
+	}
+
+=======
 			throw new ValidatorException(new FacesMessage("Deveria começar com 1"));
 		}
 	}
@@ -53,6 +78,7 @@ public class LivroBean {
 		return new DAO<Autor>(Autor.class).listaTodos();
 	}
 	
+>>>>>>> branch 'master' of https://github.com/armando-couto/JSF2_Caelum.git
 	public List<Livro> getLivros() {
 		return new DAO<Livro>(Livro.class).listaTodos();
 	}
